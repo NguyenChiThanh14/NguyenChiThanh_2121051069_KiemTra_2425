00051c0f72fde8955cf2e4abd75202a0b490b166
NguyenChiThanh14 <thanhnguyen141003a@gmail.com>
2025-05-26 13:25:43 +0700

    baikiemtra

---
+++ KiemTraMVC/Models/nguyenchithanh.cs
@@ -0,0 +1,10 @@
+namespace workspace.json;
+
+public class nguyenchithanh
+{
+    public string? ID { get; set; }
+
+    public string? email { get; set; }
+    
+     public string? HoVaTen { get; set; }
+}
